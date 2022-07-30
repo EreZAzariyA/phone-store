fc052e9bd87cb9e792a4b7f24d0e193739e23af0
Yair Azaria <44365672+yairazaria@users.noreply.github.com>
2022-07-30 13:46:08 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -26,3 +26,4 @@ export function numberWithCommas(x: number) {
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
